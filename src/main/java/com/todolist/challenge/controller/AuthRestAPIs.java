3c5d1f94c93e53c9c0af1433755b958e9b03348a
ravi <ravi@192.168.1.8>
2021-03-08 17:58:01 +0000

    Cleaning imports

--- src/main/java/com/todolist/challenge/controller/AuthRestAPIs.java
+++ src/main/java/com/todolist/challenge/controller/AuthRestAPIs.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
--- src/main/java/com/todolist/challenge/controller/TodoController.java
+++ src/main/java/com/todolist/challenge/controller/TodoController.java
@@ -1,17 +1,23 @@
 package com.todolist.challenge.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import com.todolist.challenge.model.Todo;
-import com.todolist.challenge.service.TodoService;
+import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.todolist.challenge.model.Todo;
+import com.todolist.challenge.service.TodoService;
 
 @RestController
 @RequestMapping(path = "/todos")
