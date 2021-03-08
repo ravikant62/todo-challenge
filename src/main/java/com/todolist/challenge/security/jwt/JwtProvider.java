00df2de00f453fb754ef8b11f36bbd1a5ad3e5c8
ravi <ravi@192.168.1.8>
2021-03-08 18:10:24 +0000

    Change property name

--- src/main/java/com/todolist/challenge/security/jwt/JwtProvider.java
+++ src/main/java/com/todolist/challenge/security/jwt/JwtProvider.java
@@ -28,10 +28,10 @@ public class JwtProvider {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 
-    @Value("${loizenai.app.jwtSecret}")
+    @Value("${app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${loizenai.app.jwtExpiration}")
+    @Value("${app.jwtExpiration}")
     private int jwtExpiration;
 
     public String generateJwtToken(Authentication authentication) {
